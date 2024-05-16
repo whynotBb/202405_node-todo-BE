303a5d884b89ca073b0ba9f5466e306f51d7a010
whynot-byoul <whynot.byoul@gmail.com>
2024-05-17 07:46:56 +0900

    에러메시지 보여주기 수정

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -40,8 +40,8 @@ userController.loginWithEmail = async (req, res) => {
             }
         }
         throw new Error("이메일 또는 비밀번호가 일치하지 않습니다.");
-    } catch (err) {
-        res.status(400).json({ status: "fail", err });
+    } catch (error) {
+        res.status(400).json({ status: "fail", message: error.message });
     }
 };
 
